c69622e90957a7c10e26959108feb057f9a40a8c
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-11-04 21:43:51 +0000

    QRCode imported and template added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import QRCode from "react-qr-code";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>QR Code Generator</h1>
+      <div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
+        <QRCode
+          size={256}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={value}
+          viewBox={`0 0 256 256`}
+        />
+      </div>
 
       <div>
         <div>
