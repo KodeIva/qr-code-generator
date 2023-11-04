edf26eea35923db314ea59c1d951c80d5e0de652
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-11-04 21:34:36 +0000

    Added page title, link input div, name input div, button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>QR Code Generator</h1>
+
+      <div>
+        <div>
+          <h4>Enter your link</h4>
+          <input type='text' />
+        </div>
+        <div>
+          <h4>Enter website name </h4>
+          <input type='text' />
+        </div>
+        <button>Generate</button>
+      </div>
     </div>
   );
 }
