459614314851bce73eab57517a26e186004385b9
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-11-04 22:57:45 +0000

    QRCode added, props added, created ul to display list of generated QR Codes

--- src/component/CodesList.js
+++ src/component/CodesList.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import QRCode from 'react-qr-code'
 
-const CodesList = ({name,link}) => {
+const CodesList = ({link, name}) => {
   return (
-    <div>CodesList</div>
+    <ul>
+      <li>
+       <QRCode
+          size={256}
+          style={{ height: "100px", maxWidth: "80%", width: "80%" }}
+          value={link}
+          viewBox={`0 0 256 256`}
+         ></QRCode>
+         <h4>{name}</h4>
+      </li>
+    </ul>
   )
 }
 
