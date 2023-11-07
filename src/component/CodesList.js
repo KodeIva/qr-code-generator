7c7018e819ddde7e3a1d870d688fc000849d1032
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-11-07 17:32:41 +0000

    Added styles to 'ul' and 'li'

--- src/component/CodesList.js
+++ src/component/CodesList.js
@@ -3,11 +3,11 @@ import QRCode from 'react-qr-code'
 
 const CodesList = ({link, name}) => {
   return (
-    <ul>
-      <li>
+    <ul className='mt-10 bg-yellow-300 '>
+      <li className='flex flex-col justify-center items-center bg-green-300 w-[190px] h-[180px] m-2'>
        <QRCode
           size={256}
-          style={{ height: "100px", maxWidth: "80%", width: "80%" }}
+          style={{ height: "100px", margin: '2px',padding:'1px', maxWidth: "100%", width: "100%" }}
           value={link}
           viewBox={`0 0 256 256`}
          ></QRCode>
