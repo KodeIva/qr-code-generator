232b830b298fa45c750dc9ef4e82391ac3268ed8
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-11-07 17:13:43 +0000

    Button styles added

--- src/App.js
+++ src/App.js
@@ -38,21 +38,25 @@ function App() {
       <h1 className='text-3xl text-slate-700 font-semibold pt-9 mb-9'>QR Code Generator</h1>
   
       <div className='bg-pink-200 py-8'>
-        <div className='bg-orange-100 my-3 py-4'>
+        <div className='my-3 py-4'>
           <h4 className='text-lg font-medium mb-2 text-slate-700'>Enter your link</h4>
           <input className='h-10 w-60 rounded-xl shadow-md outline-none pl-3' type='text' value={link} onChange={(e) => linkChange(e)} />
         </div>
-        <div className='bg-orange-100 my-3 py-4'>
+        <div className='my-3 py-4'>
           <h4 className='text-lg font-medium mb-2 text-slate-700'>Enter website name </h4>
           <input className='h-10 w-60 rounded-xl shadow-md outline-none pl-3' type='text' value={name} onChange={(e) => nameChange(e)} />
         </div>
-        <button onClick={generator}>Generate</button>
-        {isClicked && <div>
-          {codes.map((code) => {
-            return (
-              <CodesList link={code.link} name={code.name}/>
-            )
-          })}
+        <button
+          className=' bg-slate-600 text-white text-lg rounded-xl w-36 mt-10 py-2 hover:bg-white border-4 border-slite-100 hover:text-slate-600 hover:border-3 hover:border-slate-600 transition duration-600' 
+          onClick={generator}>
+            Generate
+        </button>
+           {isClicked && <div>
+            {codes.map((code) => {
+              return (
+               <CodesList link={code.link} name={code.name}/>
+              )
+            })}
         </div>
         
         }
