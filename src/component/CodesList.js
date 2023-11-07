1f931dac02258efadf880f9cc7542b95ed7f5d57
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-11-07 19:08:46 +0000

    'li' background color changed

--- src/component/CodesList.js
+++ src/component/CodesList.js
@@ -3,8 +3,8 @@ import QRCode from 'react-qr-code'
 
 const CodesList = ({link, name}) => {
   return (
-    <ul className='mt-10 bg-yellow-300 '>
-      <li className='flex flex-col justify-center items-center bg-green-300 w-[190px] h-[180px] m-2'>
+    <ul className='mt-10'>
+      <li className='flex flex-col justify-center items-center bg-slate-200 shadow-lg w-[190px] h-[180px] m-2'>
        <QRCode
           size={256}
           style={{ height: "100px", margin: '2px',padding:'1px', maxWidth: "100%", width: "100%" }}
