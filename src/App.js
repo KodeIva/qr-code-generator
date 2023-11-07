7856769ee0ec561287f03d3a73b0ee89a181e34d
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-11-07 19:16:36 +0000

    Link input width changed

--- src/App.js
+++ src/App.js
@@ -38,16 +38,16 @@ function App() {
       <h1 className='text-3xl text-slate-700 font-semibold pt-9 mb-9'>QR Code Generator</h1>
   
       <div className=' py-1'>
-        <div className='my-3 py-4'>
+        <div className='my-2 py-4'>
           <h4 className='text-lg font-medium mb-2 text-slate-700'>Enter your link</h4>
-          <input className='h-10 w-60 rounded-xl shadow-md outline-none pl-3' type='text' value={link} onChange={(e) => linkChange(e)} />
+          <input className='h-10 w-80 rounded-xl shadow-md outline-none pl-3' type='text' value={link} onChange={(e) => linkChange(e)} />
         </div>
-        <div className='my-3 py-4'>
+        <div className='my-2 py-4'>
           <h4 className='text-lg font-medium mb-2 text-slate-700'>Enter website name </h4>
           <input className='h-10 w-60 rounded-xl shadow-md outline-none pl-3' type='text' value={name} onChange={(e) => nameChange(e)} />
         </div>
         <button
-          className=' bg-slate-600 text-white text-lg rounded-xl w-36 mt-10 py-2 hover:bg-white border-4 border-slite-100 hover:text-slate-600 hover:border-3 hover:border-slate-600 transition duration-600' 
+          className=' bg-slate-600 text-white text-lg rounded-xl w-36 mt-5 py-2 hover:bg-white border-4 border-slite-100 hover:text-slate-600 hover:border-3 hover:border-slate-600 transition duration-600' 
           onClick={generator}>
             Generate
         </button>
